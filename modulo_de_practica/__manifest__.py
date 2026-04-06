595160fa687512f951e40a6f3165f1d1b610ebe8
Santtiag0 <168385978+Santtiag0@users.noreply.github.com>
2026-04-06 12:04:14 -0500

    ¨[ADD]

---
+++ modulo_de_practica/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Module library',
+    'version': '1.0',
+    'sumary': 'El modulo de ejemplo',
+    'author': 'SantiagoMartinez',
+    'depends': ['base'],
+    'data' : [
+        'views/Vista_Estado_Mascota.xml',   
+        'views/Vista_Lista_Mascota.xml',    
+        'views/view_busqueda_mascota.xml',  
+        'views/Menu_Mascotas.xml',
+        ],
+    
+    'installable': True,
+    'application': True,
+    
+    
+}
