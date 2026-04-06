c0026b6e25afca2eb5d9e690a04ec2b0543e0bfd
SantiagoMartinez <santiago.martinez.g2000@gmail.com>
2026-04-06 08:26:06 -0500

    estructura del modelo y manifest

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Module library',
+    'version': '1.0',
+    'sumary': 'El modulo de ejemplo',
+    'author': 'SantiagoMartinez',
+    'depends': ['base'],
+    
+    
+    'installable': True,
+    'application': True,
+    
+    
+}
