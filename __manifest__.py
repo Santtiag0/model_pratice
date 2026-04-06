a25d21732cdf6596a26b40ea1c05c2828c264fdd
Santtiag0 <168385978+Santtiag0@users.noreply.github.com>
2026-04-06 10:01:43 -0500

    modulo de mascotas

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,12 @@
     'sumary': 'El modulo de ejemplo',
     'author': 'SantiagoMartinez',
     'depends': ['base'],
-    
+    'data' : [
+        'views/Vista_Estado_Mascota.xml',   
+        'views/Vista_Lista_Mascota.xml',    
+        'views/view_busqueda_mascota.xml',  
+        'views/Menu_Mascotas.xml',
+        ],
     
     'installable': True,
     'application': True,
